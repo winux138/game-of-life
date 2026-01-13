954b6ce3ef342fa2479a4cb746a6da424cf52ade
ju <thomas.julien@protonmail.com>
2026-01-13 12:57:00 +0100

    Add Hello world from Macroquad

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "{{NAME}}";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -21,17 +21,42 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        inputs = with pkgs; [
+          openssl
+          pkg-config
+          xorg.libX11
+          xorg.libX11.dev
+          xorg.libXi
+          xorg.libXi.dev
+          xorg.libICE
+          xorg.libSM
+          xorg.libXinerama
+          fontconfig
+          libGL
+          libxkbcommon
+          glib
+          freetype
+        ];
       in
       {
         devShells.default =
           with pkgs;
           mkShell {
             buildInputs = [
-              openssl
-              pkg-config
-
               (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-            ];
+            ]
+            ++ inputs;
+
+            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath inputs}";
+
+            # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            #   pkgs.freetype
+            #   pkgs.libGL
+            #   pkgs.xorg.libX11
+            #   pkgs.xorg.libXi
+            #   pkgs.xorg.libXrandr
+            #   libxkbcommon
+            # ];
 
             shellHook = '''';
           };
